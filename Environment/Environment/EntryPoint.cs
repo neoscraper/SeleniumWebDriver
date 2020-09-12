d3d975408a7bf2cb41cceeef0df12fad0134e7c6
Thomas <thomoh777@gmail.com>
2020-09-11 20:13:00 -0600

    Setup environment for web driver

---
+++ Environment/Environment/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        //Sets browser type
+        IWebDriver driver = new ChromeDriver();
+        //Website to test
+        driver.Navigate().GoToUrl("http://testing.todorvachev.com");
+        //Wait 3 seconds and let page load and appead
+        Thread.Sleep(3000);
+        //Closes browser
+        driver.Quit();
+    }
+}
