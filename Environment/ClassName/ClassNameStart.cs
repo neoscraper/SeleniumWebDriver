028eafd3abf9002f59eeb92676836a4e9c088958
Thomas <thomoh777@gmail.com>
2020-09-14 17:50:06 -0600

    Added tests for finding selectors

---
+++ Environment/ClassName/ClassNameStart.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class NameStart
+{
+    static void Main()
+    {
+        //variables
+        string url = "https://testing.todorvachev.com/selectors/css-path/";
+        //paths that tell website where to navigate too
+        string cssPath = "#post-108 > div > figure > img";
+        string xPath = "//*[@id=\"post-108\"]/div/figure/img";
+
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(url);
+        IWebElement cssPathElement = driver.FindElement(By.CssSelector(cssPath));
+        IWebElement xPathElement = driver.FindElement(By.XPath(xPath));
+        //cssPath check
+        if (cssPathElement.Displayed)
+        {
+            ResultMessage("CSS Path Element Visible", cssPathElement.Displayed);
+        }
+        else
+        {
+            ResultMessage("CSS Path Element not visible", cssPathElement.Displayed);
+        }
+        //xPath check
+        if (xPathElement.Displayed)
+        {
+            ResultMessage("X Path Element Visible", xPathElement.Displayed);
+        }
+        else
+        {
+            ResultMessage("X Path Element not visible", xPathElement.Displayed);
+        }
+        driver.Quit();
+    }
+
+    //Change format of text based on results
+    private static void ResultMessage(string message, bool result)
+    {
+        if (result)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("SUCCESS: " + message);
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ERROR: " + message);
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
---
+++ Environment/ID Selector/IDStart.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace ID_Selector
+{
+    class IDStart
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            string url = "http://testing.todorvachev.com/selectors/name/";
+            string ID = "testImage";
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+            IWebElement IDElement = driver.FindElement(By.Id(ID));
+
+            if (IDElement.Displayed)
+            {
+                ResultMessage("ID Element Visible", IDElement.Displayed);
+            }
+            else
+            {
+                ResultMessage("ID Element not visible", IDElement.Displayed);
+            }
+
+            driver.Quit();
+        }
+
+        //Change format of text based on results
+        private static void ResultMessage(string message, bool result)
+        {
+            if (result)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("SUCCESS: " + message);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR: " + message);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
