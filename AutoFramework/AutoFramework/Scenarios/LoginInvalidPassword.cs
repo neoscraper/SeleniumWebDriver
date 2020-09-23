e04846290bcf047d0052709cbcc2773c08e85a76
Thomas <thomoh777@gmail.com>
2020-09-23 02:36:12 -0600

    Added a few comments

--- AutoFramework/AutoFramework/Scenarios/LoginInvalidPassword.cs
+++ AutoFramework/AutoFramework/Scenarios/LoginInvalidPassword.cs
@@ -16,22 +16,28 @@
         [OneTimeSetUp]
         public void Initialize()
         {
+            //Creates driver that will be interacted with
             Actions.InitializeDriver();
+            //Goes to login page
             NavigateTo.LoginFormScenarioThroughTestCases();
         }
 
         [Test]
         public void LessThan5Chars()
         {
+            //Puts in information
             Thread.Sleep(1000);
             Actions.FillLoginForm(Config.Credentials.Valid.Username,
                 Config.Credentials.Invalid.Password.FourCharacters, 
                 Config.Credentials.Invalid.Password.FourCharacters);
             Thread.Sleep(1000);
 
+            //Switches to alert in order to see result
             alert = Driver.driver.SwitchTo().Alert();
             Thread.Sleep(5000);
+            //Checks result
             Assert.AreEqual(Config.AlertsTexts.PasswordLenghtOutOfRange, alert.Text);
+            //Closes alert
             alert.Accept();
 
         }
